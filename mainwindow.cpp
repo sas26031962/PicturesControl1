776e3e5d4dd809676e7fc2100712cd0c67e1c05d
sas26031962 <sasx26031962@gmail.com>
2025-05-06 07:50:32 +0300

    Добавление регулярного выражения для поиска повёрнутых изображений

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,6 +28,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->comboBoxPatterns->addItem("^[Ii][Mm][Gg]_20[0-9]{6}_[0-9]{6}");
     ui->comboBoxPatterns->addItem("^20[0-9]{2}-[0-9]{2}-[0-9]{2} [0-9]{2}-[0-9]{2}-[0-9]{2}");
     ui->comboBoxPatterns->addItem("^20[0-9]{6}_[0-9]{6}");
+    ui->comboBoxPatterns->addItem("^.+_1$");
 
     progressBarNavigation = ui->progressBarNavigation;
     SpinBoxIndex = ui->spinBoxIndex;
