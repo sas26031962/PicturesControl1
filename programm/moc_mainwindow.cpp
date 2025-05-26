6e4278a0f8c2b5b47d1f23850c53032a28363f24
sas26031962 <sasx26031962@gmail.com>
2025-05-26 18:05:24 +0300

    Внесение данных new 238

--- programm/moc_mainwindow.cpp
+++ programm/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[57];
-    char stringdata0[1337];
+    QByteArrayData data[59];
+    char stringdata0[1432];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -88,7 +88,9 @@ QT_MOC_LITERAL(52, 1220, 18), // "execShowExecStatus"
 QT_MOC_LITERAL(53, 1239, 22), // "execShiftXValueChanged"
 QT_MOC_LITERAL(54, 1262, 22), // "execShiftYValueChanged"
 QT_MOC_LITERAL(55, 1285, 47), // "execListWidgetSubjectCustomCo..."
-QT_MOC_LITERAL(56, 1333, 3) // "pos"
+QT_MOC_LITERAL(56, 1333, 3), // "pos"
+QT_MOC_LITERAL(57, 1337, 45), // "execListWidgetPlaceCustomCont..."
+QT_MOC_LITERAL(58, 1383, 48) // "execListWidgetPropertyCustomC..."
 
     },
     "MainWindow\0draw\0\0s\0showExecStatus\0"
@@ -128,7 +130,8 @@ QT_MOC_LITERAL(56, 1333, 3) // "pos"
     "execSpinBoxAngle\0angle\0execShowExecStatus\0"
     "execShiftXValueChanged\0execShiftYValueChanged\0"
     "execListWidgetSubjectCustomContextMenuRequested\0"
-    "pos"
+    "pos\0execListWidgetPlaceCustomContextMenuRequested\0"
+    "execListWidgetPropertyCustomContextMenuRequested"
 };
 #undef QT_MOC_LITERAL
 
@@ -138,7 +141,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      51,   14, // methods
+      53,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -146,59 +149,61 @@ static const uint qt_meta_data_MainWindow[] = {
        2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,  269,    2, 0x06 /* Public */,
-       4,    1,  272,    2, 0x06 /* Public */,
+       1,    1,  279,    2, 0x06 /* Public */,
+       4,    1,  282,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       5,    0,  275,    2, 0x08 /* Private */,
-       6,    0,  276,    2, 0x08 /* Private */,
-       7,    0,  277,    2, 0x08 /* Private */,
-       8,    0,  278,    2, 0x08 /* Private */,
-       9,    0,  279,    2, 0x08 /* Private */,
-      10,    0,  280,    2, 0x08 /* Private */,
-      11,    0,  281,    2, 0x08 /* Private */,
-      12,    0,  282,    2, 0x08 /* Private */,
-      13,    0,  283,    2, 0x08 /* Private */,
-      14,    0,  284,    2, 0x08 /* Private */,
-      15,    0,  285,    2, 0x08 /* Private */,
-      16,    0,  286,    2, 0x08 /* Private */,
-      17,    0,  287,    2, 0x08 /* Private */,
-      18,    0,  288,    2, 0x08 /* Private */,
-      19,    1,  289,    2, 0x08 /* Private */,
-      21,    0,  292,    2, 0x08 /* Private */,
-      22,    0,  293,    2, 0x08 /* Private */,
-      23,    0,  294,    2, 0x08 /* Private */,
-      24,    0,  295,    2, 0x08 /* Private */,
-      25,    0,  296,    2, 0x08 /* Private */,
-      26,    0,  297,    2, 0x08 /* Private */,
-      27,    0,  298,    2, 0x08 /* Private */,
-      28,    0,  299,    2, 0x08 /* Private */,
-      29,    0,  300,    2, 0x08 /* Private */,
-      30,    0,  301,    2, 0x08 /* Private */,
-      31,    0,  302,    2, 0x08 /* Private */,
-      32,    0,  303,    2, 0x08 /* Private */,
-      33,    0,  304,    2, 0x08 /* Private */,
-      34,    0,  305,    2, 0x08 /* Private */,
-      35,    0,  306,    2, 0x08 /* Private */,
-      36,    0,  307,    2, 0x08 /* Private */,
-      37,    0,  308,    2, 0x08 /* Private */,
-      38,    0,  309,    2, 0x08 /* Private */,
-      39,    0,  310,    2, 0x08 /* Private */,
-      40,    0,  311,    2, 0x08 /* Private */,
-      41,    0,  312,    2, 0x08 /* Private */,
-      42,    0,  313,    2, 0x08 /* Private */,
-      43,    0,  314,    2, 0x08 /* Private */,
-      44,    0,  315,    2, 0x08 /* Private */,
-      45,    0,  316,    2, 0x08 /* Private */,
-      46,    0,  317,    2, 0x08 /* Private */,
-      47,    0,  318,    2, 0x08 /* Private */,
-      48,    0,  319,    2, 0x08 /* Private */,
-      49,    0,  320,    2, 0x08 /* Private */,
-      50,    1,  321,    2, 0x08 /* Private */,
-      52,    1,  324,    2, 0x08 /* Private */,
-      53,    0,  327,    2, 0x08 /* Private */,
-      54,    0,  328,    2, 0x08 /* Private */,
-      55,    1,  329,    2, 0x08 /* Private */,
+       5,    0,  285,    2, 0x08 /* Private */,
+       6,    0,  286,    2, 0x08 /* Private */,
+       7,    0,  287,    2, 0x08 /* Private */,
+       8,    0,  288,    2, 0x08 /* Private */,
+       9,    0,  289,    2, 0x08 /* Private */,
+      10,    0,  290,    2, 0x08 /* Private */,
+      11,    0,  291,    2, 0x08 /* Private */,
+      12,    0,  292,    2, 0x08 /* Private */,
+      13,    0,  293,    2, 0x08 /* Private */,
+      14,    0,  294,    2, 0x08 /* Private */,
+      15,    0,  295,    2, 0x08 /* Private */,
+      16,    0,  296,    2, 0x08 /* Private */,
+      17,    0,  297,    2, 0x08 /* Private */,
+      18,    0,  298,    2, 0x08 /* Private */,
+      19,    1,  299,    2, 0x08 /* Private */,
+      21,    0,  302,    2, 0x08 /* Private */,
+      22,    0,  303,    2, 0x08 /* Private */,
+      23,    0,  304,    2, 0x08 /* Private */,
+      24,    0,  305,    2, 0x08 /* Private */,
+      25,    0,  306,    2, 0x08 /* Private */,
+      26,    0,  307,    2, 0x08 /* Private */,
+      27,    0,  308,    2, 0x08 /* Private */,
+      28,    0,  309,    2, 0x08 /* Private */,
+      29,    0,  310,    2, 0x08 /* Private */,
+      30,    0,  311,    2, 0x08 /* Private */,
+      31,    0,  312,    2, 0x08 /* Private */,
+      32,    0,  313,    2, 0x08 /* Private */,
+      33,    0,  314,    2, 0x08 /* Private */,
+      34,    0,  315,    2, 0x08 /* Private */,
+      35,    0,  316,    2, 0x08 /* Private */,
+      36,    0,  317,    2, 0x08 /* Private */,
+      37,    0,  318,    2, 0x08 /* Private */,
+      38,    0,  319,    2, 0x08 /* Private */,
+      39,    0,  320,    2, 0x08 /* Private */,
+      40,    0,  321,    2, 0x08 /* Private */,
+      41,    0,  322,    2, 0x08 /* Private */,
+      42,    0,  323,    2, 0x08 /* Private */,
+      43,    0,  324,    2, 0x08 /* Private */,
+      44,    0,  325,    2, 0x08 /* Private */,
+      45,    0,  326,    2, 0x08 /* Private */,
+      46,    0,  327,    2, 0x08 /* Private */,
+      47,    0,  328,    2, 0x08 /* Private */,
+      48,    0,  329,    2, 0x08 /* Private */,
+      49,    0,  330,    2, 0x08 /* Private */,
+      50,    1,  331,    2, 0x08 /* Private */,
+      52,    1,  334,    2, 0x08 /* Private */,
+      53,    0,  337,    2, 0x08 /* Private */,
+      54,    0,  338,    2, 0x08 /* Private */,
+      55,    1,  339,    2, 0x08 /* Private */,
+      57,    1,  342,    2, 0x08 /* Private */,
+      58,    1,  345,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::QString,    3,
@@ -253,6 +258,8 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::QString,    3,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::QPoint,   56,
+    QMetaType::Void, QMetaType::QPoint,   56,
     QMetaType::Void, QMetaType::QPoint,   56,
 
        0        // eod
@@ -315,6 +322,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 48: _t->execShiftXValueChanged(); break;
         case 49: _t->execShiftYValueChanged(); break;
         case 50: _t->execListWidgetSubjectCustomContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 51: _t->execListWidgetPlaceCustomContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 52: _t->execListWidgetPropertyCustomContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -365,13 +374,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 51)
+        if (_id < 53)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 51;
+        _id -= 53;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 51)
+        if (_id < 53)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 51;
+        _id -= 53;
     }
     return _id;
 }
