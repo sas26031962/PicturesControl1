0aec58bb861d18164193f8cc2e4955bfc89a6dfb
sas26031962 <sasx26031962@gmail.com>
2025-06-26 16:45:49 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ programm/moc_cactionsexec.cpp
@@ -0,0 +1,166 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'cactionsexec.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../cactionsexec.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'cactionsexec.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.8. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_cActionsExec_t {
+    QByteArrayData data[11];
+    char stringdata0[163];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_cActionsExec_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_cActionsExec_t qt_meta_stringdata_cActionsExec = {
+    {
+QT_MOC_LITERAL(0, 0, 12), // "cActionsExec"
+QT_MOC_LITERAL(1, 13, 18), // "execShowExecStatus"
+QT_MOC_LITERAL(2, 32, 0), // ""
+QT_MOC_LITERAL(3, 33, 1), // "s"
+QT_MOC_LITERAL(4, 35, 21), // "execActionRemoveMovie"
+QT_MOC_LITERAL(5, 57, 1), // "x"
+QT_MOC_LITERAL(6, 59, 20), // "execActionRemoveText"
+QT_MOC_LITERAL(7, 80, 19), // "execActionRemoveTif"
+QT_MOC_LITERAL(8, 100, 19), // "execActionRemoveBin"
+QT_MOC_LITERAL(9, 120, 19), // "execActionRemove3gp"
+QT_MOC_LITERAL(10, 140, 22) // "execActionShowNewFiles"
+
+    },
+    "cActionsExec\0execShowExecStatus\0\0s\0"
+    "execActionRemoveMovie\0x\0execActionRemoveText\0"
+    "execActionRemoveTif\0execActionRemoveBin\0"
+    "execActionRemove3gp\0execActionShowNewFiles"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_cActionsExec[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   49,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    1,   52,    2, 0x0a /* Public */,
+       6,    1,   55,    2, 0x0a /* Public */,
+       7,    1,   58,    2, 0x0a /* Public */,
+       8,    1,   61,    2, 0x0a /* Public */,
+       9,    1,   64,    2, 0x0a /* Public */,
+      10,    1,   67,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Bool,    5,
+    QMetaType::Void, QMetaType::Bool,    5,
+    QMetaType::Void, QMetaType::Bool,    5,
+    QMetaType::Void, QMetaType::Bool,    5,
+    QMetaType::Void, QMetaType::Bool,    5,
+    QMetaType::Void, QMetaType::Bool,    5,
+
+       0        // eod
+};
+
+void cActionsExec::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<cActionsExec *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->execShowExecStatus((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->execActionRemoveMovie((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 2: _t->execActionRemoveText((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 3: _t->execActionRemoveTif((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 4: _t->execActionRemoveBin((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 5: _t->execActionRemove3gp((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 6: _t->execActionShowNewFiles((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (cActionsExec::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&cActionsExec::execShowExecStatus)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject cActionsExec::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_cActionsExec.data,
+    qt_meta_data_cActionsExec,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *cActionsExec::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *cActionsExec::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_cActionsExec.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int cActionsExec::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 7;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void cActionsExec::execShowExecStatus(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
